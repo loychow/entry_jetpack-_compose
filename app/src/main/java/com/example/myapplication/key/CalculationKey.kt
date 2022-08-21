798280a557d0f6e75a3b3d7d25ee83aacf096c11
zack <choulizhong@outlook.com>
2022-08-21 18:21:03 +0800

    修改内容： 完善小数点拼接

--- app/src/main/java/com/example/myapplication/key/CalculationKey.kt
+++ app/src/main/java/com/example/myapplication/key/CalculationKey.kt
@@ -1,13 +1,16 @@
 package com.example.myapplication.key
 
-import android.util.Log
 import com.example.myapplication.ui.viewmodle.Record
 
 class CalculationKey(private val symbol: String = "=") : Key {
     override fun exercise(record: Record): Record {
         record.offsetOne()
         val r: Double = record.opt.arithmetic.execute(record)
-        return record.copy(result = r, display = r.toString())
+        return record.copy(
+            result = r,
+            display = r.toString(),
+            shouldReset = true
+        )
     }
 
     override fun getSymbol(): String {
--- app/src/main/java/com/example/myapplication/key/NumericKey.kt
+++ app/src/main/java/com/example/myapplication/key/NumericKey.kt
@@ -4,27 +4,23 @@ import com.example.myapplication.ui.viewmodle.Record
 
 class NumericKey(private val symbol: String = "0") : Key {
     override fun exercise(record: Record): Record {
-        var currentDisplay: Double = record.display.toDouble()
+        var currentDisplay: String = record.display
         if (record.shouldReset) {
-            currentDisplay = 0.0
-            record.shouldReset=false
+            currentDisplay = "0"
+            record.shouldReset = false
         }
-        return record.copy(display = count(currentDisplay).toString())
+        return record.copy(display = count(currentDisplay))
     }
 
     override fun getSymbol(): String {
         return symbol
     }
 
-    private fun count(display: Double): Double {
-        var displayNum = display
-        displayNum = if (displayNum == 0.0) {
-            symbol.toDouble()
-        } else if (displayNum > 0) {
-            displayNum * 10 + symbol.toDouble()
+    private fun count(display: String): String {
+        return if (display == "0") {
+            symbol
         } else {
-            displayNum * 10 - symbol.toDouble()
+            display + symbol
         }
-        return displayNum
     }
 }
